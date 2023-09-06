120d5110e83fbfce320676c59b1483ea1a07746a
Roopesh Singh <roopesh@navya.net>
2023-09-06 07:19:29 +0530

    added jsx code instead React.createElement

--- App.js
+++ App.js
@@ -3,13 +3,22 @@ import ReactDOM from "react-dom/client";
 
 // React Elements just like to dom elements
 // React.createElement => Object => HtmlElements(render)
+// React.createElement => ReactElement- JS Object => HTMLElements(render)
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namaste React by Roopesh 🚀 "
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namaste React by Roopesh 🚀📖"
+// );
+
+// console.log(heading);
+// JSX syntax is like HTML
+// JSX => React.createElement =  ReactElement - JS Object => HTMLElement(render)
+//JSX (Transpiled before it reaches the JS) Parcel - Babel
+const jsxHeading = <h1 id="heading">Hello from Namaste React By Roopesh 🚀</h1>;
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
